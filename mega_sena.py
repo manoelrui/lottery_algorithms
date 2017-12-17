664cbad7b27d821cc265cdcc206fb26badbcd50a
manoelrui <manoelrui19@gmail.com>
2017-12-17 15:32:11 -0300

    Added less frequent algorithm

--- mega_sena.py
+++ mega_sena.py
@@ -9,7 +9,6 @@ def random(historico):
 def fixo(historico):
 	return np.array([39,9,37,49,43,41])
 
-
 def most_frequent(historic):
     freq = [[i, 0] for i in range(1, MAX_UNIT_NUMBER + 1)]
     for lottery_draw in historic:
@@ -21,7 +20,18 @@ def most_frequent(historic):
     sorted_by_freq = sorted(freq, key=lambda tup: tup[1], reverse=True)
     return np.array([sorted_by_freq[i][0] for i in range(0, MAX_GUESS_NUMBERS)])
 
+def less_frequent(historic):
+    freq = [[i, 0] for i in range(1, MAX_UNIT_NUMBER + 1)]
+    for lottery_draw in historic:
+        for number in lottery_draw.numeros:
+            for counter in freq:
+                if counter[0] == number:
+                    counter[1] += 1
+
+    sorted_by_freq = sorted(freq, key=lambda tup: tup[1])
+    return np.array([sorted_by_freq[i][0] for i in range(0, MAX_GUESS_NUMBERS)])
+
 
 
 sorteios = carregar_sorteios('sena_parsed.csv')
-jogar(sorteios,most_frequent)
+jogar(sorteios,less_frequent)
